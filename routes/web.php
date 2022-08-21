4f9187510ba393b9d5fe23f1d5c6abf6a983a08c
Guillermo García <guillermieres97@gmail.com>
2022-08-21 17:31:50 +0200

    create route to products view

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ProductsController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', [ProductsController::class, 'index']);
+   /*  ->name('products.index'); */
